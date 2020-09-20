9920f0bf162b76c6507e35584dd0e27b31b257bc
Whee <gnltjd2376@gmail.com>
2020-09-20 13:09:25 +0900

    2020-09-20

--- src/whee/cote/OddEven.java
+++ src/whee/cote/OddEven.java
@@ -3,6 +3,7 @@ package whee.cote;
 public class OddEven {
 	public static void main(String[] args) {
 		OddEven(3);
+		System.out.println(OddEven(4));
 	}
 	
 	public static String OddEven(int num) {
