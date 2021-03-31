7beffee1e45a9678626f12327223f61eb072b243
Whee <gnltjd2376@gmail.com>
2021-03-31 15:41:31 +0900

    20210331

---
+++ src/whee/cote/homework2.java
@@ -0,0 +1,11 @@
+package whee.cote;
+
+import java.util.Scanner;
+
+public class homework2{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+	}
+}
