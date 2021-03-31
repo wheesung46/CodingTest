d49ca8d80f53870a8ddddedd165b64df933b22ec
Whee <gnltjd2376@gmail.com>
2021-03-31 17:11:26 +0900

    add problem10

--- src/whee/cote/problem9.java
+++ src/whee/cote/problem9.java
@@ -7,11 +7,15 @@ public class problem9{
 		Scanner num1 = new Scanner(System.in);
 		int a = num1.nextInt();
 		int b = num1.nextInt();
-		System.out.println(add(a,b));
-		System.out.println(sub(a,b));
-		System.out.println(mul(a,b));
-		System.out.println(dev(a,b));
-		System.out.println(trash(a,b));
+		if(a>=1 && a<=10000 && b>=1 && b <=10000) {
+			System.out.println(add(a,b));
+			System.out.println(sub(a,b));
+			System.out.println(mul(a,b));
+			System.out.println(dev(a,b));
+			System.out.println(trash(a,b));
+		}
+		else
+			System.out.println("두 수는 1보다 크거나같고 10000 보다 작거나같아야 합니다.");
 	}
 	
 	public static int add(int a,int b) {
