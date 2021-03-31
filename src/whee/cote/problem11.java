773d236a54246fa76e93643266bd42adf6ae5d18
Whee <gnltjd2376@gmail.com>
2021-03-31 17:29:03 +0900

    add problem11

---
+++ src/whee/cote/problem11.java
@@ -0,0 +1,20 @@
+package whee.cote;
+
+import java.util.Scanner;
+
+public class problem11 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		int a = num1 * (num2%10);
+		int b = num1 * ((num2/10)%10);
+		int c = num1 * (num2/100);
+		int d = num1 * num2;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+}
