8514055ff10a950dffacdbfe8b22c101acdae5f5
Whee <gnltjd2376@gmail.com>
2021-04-01 16:55:16 +0900

    2021-04-01 if3

---
+++ src/whee/cote/Ifproblem3.java
@@ -0,0 +1,23 @@
+package whee.cote;
+
+import java.util.Scanner;
+
+public class Ifproblem3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		if(year>=1 && year<=4000) {
+			if(year%4==0) {
+				if(year%100!=0 || year%400==0) {
+					System.out.println("1");
+				}
+				else
+					System.out.println("0");
+			}
+			else
+				System.out.println("0");
+		}
+		else
+			System.out.println("범위를 벗어났습니다.");
+	}
+}
