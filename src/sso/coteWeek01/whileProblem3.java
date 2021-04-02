9c0af48fd80260ec1c72099dd4b2ef444c00c570
baesohyun <brty7894@naver.com>
2021-04-02 17:54:27 +0900

    04/02 17:25

--- src/sso/coteWeek01/whileProblem3.java
+++ src/sso/coteWeek01/whileProblem3.java
@@ -1,9 +1,14 @@
 package sso.coteWeek01;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class whileProblem3 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// 0보다 크거나 같고, 99보다 작거나 같은 정수가 주어질 때 다음과 같은 연산을 할 수 있다. 
 		// 먼저 주어진 수가 10보다 작다면 앞에 0을 붙여 두 자리 수로 만들고, 각 자리의 숫자를 더한다.  
 		// 그 다음, 주어진 수의 가장 오른쪽 자리 수와 앞에서 구한 합의 가장 오른쪽 자리 수를 이어 붙이면 새로운 수를 만들 수 있다. 다음 예를 보자.
@@ -15,17 +20,18 @@ public class whileProblem3 {
 		
 		// 첫째 줄에 N이 주어진다. N은 0보다 크거나 같고, 99보다 작거나 같은 정수이다.
 		
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int tmp=0;
 		
 		while(true) {
-			int N = sc.nextInt(); // 0<=N<=99
+			int N = Integer.parseInt(br.readLine()); // 0<=N<=99
 			
 			if(N>=0 && N<=99) {
 				
-				if(N<10) {
-					tmp=N;
+				if(N<10) { // N이 10보다 작으면
+					
 				}
 				
 			}else {
